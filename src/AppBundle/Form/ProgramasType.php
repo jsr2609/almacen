ef7fe06a66c4a892fd0756151a6a76d28f3699b7
Jeronimo <jsr2609@gmail.com>
2015-03-11 13:35:10 -0600

    Se termino el popup de buscar programa, se inician pruebas con los datos de las tablas.

---
+++ src/AppBundle/Form/ProgramasType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ProgramasType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('clave')
+            ->add('activo')
+            ->add('fechaCreacion')
+            ->add('fechaActualizacion')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Programas'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'appbundle_programas';
+    }
+}
