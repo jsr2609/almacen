fe6521dfd6bafde03aa9eb7ab6bbbf373d883c4a
Jeronimo <jsr2609@gmail.com>
2015-03-12 09:38:10 -0600

    Se agregaron los campos extras a la entidad Destinos

--- src/AppBundle/Entity/Destinos.php
+++ src/AppBundle/Entity/Destinos.php
@@ -16,9 +16,24 @@ class Destinos
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
-
+    
+    /**
+     * @ORM\Column(name="Clave", type="string", length=5, nullable=false, unique=true)
+     */
+    private $clave;
+    
     /**
      * @ORM\Column(name="Nombre", type="string", length=150, nullable=false)
      */
     private $nombre;
+    
+    /**
+     * @ORM\Column(name="Nombre1", type="string", length=150, nullable=true)
+     */
+    private $nombre1;
+    
+    /**
+     * @ORM\Column(name="Nombre2", type="string", length=150, nullable=true)
+     */
+    private $nombre3;
 }
