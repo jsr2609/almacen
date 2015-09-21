ec522950b175731455a3a87f04f42ac1a5022ed9
Jeronimo <jsr2609@gmail.com>
2015-09-21 15:48:06 -0500

    Se agregaron las vistas VwEntradas y VwArticulos en la carpeta SQL del proyecto

--- SQL/VwArticulos.sql
+++ SQL/VwArticulos.sql
@@ -1,15 +1,19 @@
-CREATE OR REPLACE
-    ALGORITHM = UNDEFINED 
-    DEFINER = `root`@`localhost` 
-    SQL SECURITY DEFINER
-VIEW `dbalmacen`.`VwArticulos` AS
-    SELECT 
-        `ats`.`Id` AS `Id`,
-        ats.Clave,
-        ats.Nombre,
-        ats.PartidaId,
-        pts.nombre
-        
-    FROM
-        `dbalmacen`.`Articulos` AS `ats`
-	INNER JOIN Partidas AS pts ON (ats.PartidaId = pts.Id)
+-- DROP VIEW vwarticulos;
+
+CREATE OR REPLACE VIEW vwarticulos AS 
+ SELECT ats.id,
+    ats.clave,
+    ats.nombre,
+    ats.presentacionnombre AS articulopresentacionnombre,
+    ats.partidaid,
+    pts.clave AS partidaclave,
+    pts.nombre AS partidanombre,
+    ats.presentacionid,
+    pss.nombre AS presentacionnombre,
+    ats.activo
+   FROM articulos ats
+     LEFT JOIN partidas pts ON ats.partidaid = pts.id
+     LEFT JOIN presentaciones pss ON ats.presentacionid = pss.id;
+
+ALTER TABLE vwarticulos
+  OWNER TO almacen;
--- SQL/VwEntradas.sql
+++ SQL/VwEntradas.sql
@@ -1,25 +1,25 @@
-use dbalmacen;
-CREATE OR REPLACE
-    ALGORITHM = UNDEFINED 
-    DEFINER = `root`@`localhost` 
-    SQL SECURITY DEFINER
-VIEW `dbalmacen`.`VwEntradas` AS
-    SELECT 
-        `ets`.`Id` AS `Id`,
-        ets.Folio,
-        ets.Fecha,
-        ets.TipoEntradaId,
-        ets.PedidoNumero,
-        ets.FacturaNumero,
-        ets.EjercicioId,
-        ets.ProgramaId,
-        pgs.Clave AS ProgramaClave,
-        pgs.Nombre AS ProgramaNombre,
-        ets.ProveedorId,
-        pvs.Rfc as ProveedorRfc,
-        pvs.Nombre AS ProveedorNombre,
-        ets.Activa
-    FROM
-        `dbalmacen`.`Entradas` `ets`
-    INNER JOIN Programas AS pgs ON (ets.ProgramaId = pgs.Id)
-    INNER JOIN Proveedores AS pvs ON (ets.ProveedorId = pvs.id)
+-- View: vwentradas
+
+-- DROP VIEW vwentradas;
+
+CREATE OR REPLACE VIEW vwentradas AS 
+ SELECT ets.id,
+    ets.folio,
+    ets.fecha,
+    ets.tipocompra AS tipocompraid,
+    ets.pedidonumero,
+    ets.facturanumero,
+    ets.ejercicioid,
+    ets.programaid,
+    pgs.clave AS programaclave,
+    pgs.nombre AS programanombre,
+    ets.proveedorid,
+    pvs.rfc AS proveedorrfc,
+    pvs.nombre AS proveedornombre,
+    ets.activa
+   FROM entradas ets
+     JOIN programas pgs ON ets.programaid = pgs.id
+     JOIN proveedores pvs ON ets.proveedorid = pvs.id;
+
+ALTER TABLE vwentradas
+  OWNER TO almacen;
