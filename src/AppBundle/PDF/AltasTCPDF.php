ec139cefdba189e94ffe288c2b95a6f407b9d492
Jeronimo <jsr2609@gmail.com>
2015-09-30 15:46:25 -0500

    Se eliminó el BaseAltaPDF

--- src/AppBundle/PDF/AltasTCPDF.php
+++ src/AppBundle/PDF/AltasTCPDF.php
@@ -131,10 +131,30 @@ class AltasTCPDF extends MyTCPDF
                 
 	}
     
-    public function getFullPageWidth() {
-        $margins = $this->getMargins();
+    public function init(MyTCPDF $pdf, $string = null, $footerText = null)
+    {
+        if($footerText){
+            $pdf->setFooterText($footerText);
+        }
+        $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, 
+            mb_strtoupper($string, 'UTF-8')
+        );
+        $pdf->setFooterData(array(0,64,0), array(0,64,128));
+        // set header and footer fonts
+        $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+        $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+        // set default monospaced font
+        $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+        // set margins
+        $pdf->SetMargins(PDF_MARGIN_RIGHT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+        $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+        $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+        // set auto page breaks
+        $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
         
-        return $this->getPageWidth() - $margins['left'] - $margins['right'];
+        $pdf->AddPage();
     }
     
 }
